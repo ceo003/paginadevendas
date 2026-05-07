9a7978a04395314684d7b94ed400573926b505de
Gemilson Sarajabo <gemilson.sarajabo1@gmail.com>
2026-05-07 14:21:03 +0200

    Versao oficial3

--- api/pagar.js
+++ api/pagar.js
@@ -20,10 +20,10 @@ export default async function handler(req, res) {
       },
       body: JSON.stringify({
         amount: "245.00",
-        reference: `REC${Date.now()}`, // Removi o hífen para conter apenas letras e números
+        reference: `REC${Date.now()}`,
         description: "Receita do Cha Natural",
         method: method,
-        return_url: "https://lojasolucion.online/api/download"
+        return_url: "https://lojasolucion.online" // Alterado para a raiz do site para teste de validação
       })
     });
 
